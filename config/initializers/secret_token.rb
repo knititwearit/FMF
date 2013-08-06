2877245af40f463d9ab29e2f3dcfdeef5deb5bfd
knititwearit <knititwearit@gmail.com>
2013-08-06 09:39:33 +0000

    Initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FMF::Application.config.secret_key_base = '6709c8d4cdd506bcd2fa246bf79261796daae52bed88a2dac0c0a934e61c4500443370137a152d6a085fb4fdda8d2a0c9e52623e9be6d7109b377cca9b07d271'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FMF::Application.config.session_store :cookie_store, key: '_FMF_session'
