8147aed901b32db0e26745dd1ee2f2657c5923d2
knititwearit <knititwearit@gmail.com>
2013-08-08 13:30:40 +0000

    Added partials and Listing 5.26

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 FMF::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   resources :microposts
 
   resources :users
@@ -12,6 +12,7 @@ FMF::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root  'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
