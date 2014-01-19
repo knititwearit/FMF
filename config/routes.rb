b3cf8fa58b03c270720655b635445fb73227c92a
knititwearit <knititwearit@gmail.com>
2014-01-19 14:30:48 +0000

    Fixing boiler plate text

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ FMF::Application.routes.draw do
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 end  
